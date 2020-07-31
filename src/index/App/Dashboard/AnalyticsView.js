b96963ab1feecc89fd71d50e04ca4f4daeb3b64f
gachez <briangach98@gmail.com>
2020-07-31 10:22:49 +0300

    added the responsiveness and removed the bar graph

--- src/index/App/Dashboard/AnalyticsView.js
+++ src/index/App/Dashboard/AnalyticsView.js
@@ -140,11 +140,6 @@ export default class AnalyticsView extends React.Component{
                     </CardDeck>
                     <br />
                     <br />
-                    <br />
-                    <CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-        
      
                 </div>
                            </>
